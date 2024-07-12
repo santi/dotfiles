b382d422816cd12ede3f1ce5c75e62ceb46bb6c1
Vemund Santi <vemund@santi.no>
2024-07-12 08:58:58 +0200

    Prevent icons bouncing from hidden Dock

--- mac/install.sh
+++ mac/install.sh
@@ -107,6 +107,7 @@ defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool fa
 # Hide Dock
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 1000
+defaults write com.apple.dock no-bouncing -bool true
 
 killall Dock WindowManager
 
